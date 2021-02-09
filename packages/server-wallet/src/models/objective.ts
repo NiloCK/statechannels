ff2f5625e5cf14e677ca7f2d9f504ad26182b019
geoknee <georgeknee@googlemail.com>
2021-02-09 12:10:03 +0000

    ObjectiveModel.insert -> ensure
    and accepts a generic type

--- packages/server-wallet/src/models/objective.ts
+++ packages/server-wallet/src/models/objective.ts
@@ -122,7 +122,7 @@ export class ObjectiveModel extends Model {
     };
   }
 
-  static async insert(
+  static async ensure<O extends DBObjective>(
     objectiveToBeStored: SupportedObjective & {
       status: 'pending' | 'approved' | 'rejected' | 'failed' | 'succeeded';
     },
@@ -155,7 +155,7 @@ export class ObjectiveModel extends Model {
               .ignore() // this makes it an upsert
         )
       );
-      return model.toObjective();
+      return model.toObjective<O>();
     });
   }
 
@@ -205,11 +205,20 @@ export class ObjectiveModel extends Model {
     return (await ObjectiveModel.query(tx).findByIds(objectiveIds)).map(m => m.toObjective());
   }
 
-  toObjective(): DBObjective {
-    return {
-      ...this,
-      participants: [],
-      data: this.data as any, // Here we will trust that the row respects our types
+  toObjective<O extends DBObjective = DBObjective>(): O {
+    const withParticipants = {
+      type: this.type,
+      data: this.data,
+      participants: [] as DBObjective['participants'],
     };
+    switch (this.type) {
+      case 'CloseChannel':
+      case 'DefundChannel':
+      case 'OpenChannel':
+      case 'SubmitChallenge':
+        return withParticipants as O;
+      default:
+        throw Error('unimplemented');
+    }
   }
 }
